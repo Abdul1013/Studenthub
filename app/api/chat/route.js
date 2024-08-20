140485f766612afe1e6ab59e66a3dfdc8a409e30
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2024-08-20 10:06:51 +0100

    removed stripe functionality

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,1 +1,79 @@
-import {}
+import { NextResponse } from "next/server";
+import { Pinecone } from "@pinecone-database/pinecone";
+import OpenAI from "openai";
+import { Stream } from "@mui/icons-material";
+
+const systemPrompt = `
+You are a rate my professor agent to help students find classes, that takes in user questions and answers them.
+For every user question, the top 3 professors that match the user question are returned.
+Use them to answer the question if needed.
+`;
+
+export async function POST(req) {
+  const data = await req.json();
+
+  //innitializing API's
+  const pc = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY,
+  });
+  const index = pc.index("rag").namespace("ns1");
+  const openai = new OpenAI();
+
+  //procccesiing user query
+  const text = data[data.length - 1].content;
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: text,
+    encoding_format: "float",
+  });
+
+  // querying pinecone
+  const results = await index.query({
+    topK: 5,
+    includeMetadata: true,
+    vector: embedding.data[0].embedding,
+  });
+
+  // format result
+  let resultString = "";
+  results.matches.forEach((match) => {
+    resultString += `
+  Returned Results:
+  Professor: ${match.id}
+  Review: ${match.metadata.stars}
+  Subject: ${match.metadata.subject}
+  Stars: ${match.metadata.stars}
+  \n\n`;
+  });
+
+  //combining user responese with pinceone result
+  const lastMessage = data[data.length - 1];
+  const lastMessageContent = lastMessage.content + resultString;
+  const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
+
+  //chat completion request
+  const completion = await openai.chat.comletions.create({
+    messages : [
+        {role: 'system', content: systemPrompt},
+        ...lastDataWithoutLastMessage,
+        {role: 'user', content: lastMessageContent},
+    ],
+    model : 'gpt-3.5-turbo',
+    Stream: true,
+  })
+
+  const Stream = new ReadableStream({
+    async start (controller) {
+        const enconder = new TextEncoder()
+        try {
+            for await (const chunk of completion) {
+                const content = chunk.choice[0]?.delta?.content
+                if(content){
+                    
+                }
+            }
+        }
+        
+    }
+  })
+}
--- app/result/page.js
+++ app/result/page.js
@@ -1,127 +1,127 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import getStripe from "@/utils/get-stripe";
-import {
-  Box,
-  CircularProgress,
-  Container,
-  Typography,
-  Button,
-} from "@mui/material";
-import { format } from "date-fns";
-import { Suspense } from "react";
+// "use client";
+// import { useEffect, useState } from "react";
+// import { useRouter, useSearchParams } from "next/navigation";
+// // import getStripe from "@/utils/get-stripe";
+// import {
+//   Box,
+//   CircularProgress,
+//   Container,
+//   Typography,
+//   Button,
+// } from "@mui/material";
+// import { format } from "date-fns";
+// import { Suspense } from "react";
 
-const ResultPageContent = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const session_id = searchParams.get("session_id");
+// const ResultPageContent = () => {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+//   const session_id = searchParams.get("session_id");
 
-  const [loading, setLoading] = useState(true);
-  const [session, setSession] = useState(null);
-  const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [session, setSession] = useState(null);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchCheckoutSession = async () => {
-      if (!session_id) return;
+//   useEffect(() => {
+//     const fetchCheckoutSession = async () => {
+//       if (!session_id) return;
 
-      try {
-        const res = await fetch(
-          `/api/checkout_sessions?session_id=${session_id}`
-        );
+//       try {
+//         const res = await fetch(
+//           `/api/checkout_sessions?session_id=${session_id}`
+//         );
 
-        const sessionData = await res.json();
-        if (res.ok) {
-          setSession(sessionData);
-        } else {
-          setError(sessionData.error || "Failed to fetch session data.");
-        }
-      } catch (err) {
-        setError("An error occurred while fetching the session data.");
-      } finally {
-        setLoading(false);
-      }
-    };
+//         const sessionData = await res.json();
+//         if (res.ok) {
+//           setSession(sessionData);
+//         } else {
+//           setError(sessionData.error || "Failed to fetch session data.");
+//         }
+//       } catch (err) {
+//         setError("An error occurred while fetching the session data.");
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
 
-    fetchCheckoutSession();
-  }, [session_id]);
+//     fetchCheckoutSession();
+//   }, [session_id]);
 
-  if (loading) {
-    return (
-      <Container maxWidth="100vw" sx={{ textAlign: "center", mt: 4 }}>
-        <CircularProgress />
-        <Typography variant="h6">Loading...</Typography>
-      </Container>
-    );
-  }
+//   if (loading) {
+//     return (
+//       <Container maxWidth="100vw" sx={{ textAlign: "center", mt: 4 }}>
+//         <CircularProgress />
+//         <Typography variant="h6">Loading...</Typography>
+//       </Container>
+//     );
+//   }
 
-  if (error) {
-    return (
-      <Container maxWidth="100vw" sx={{ textAlign: "center", mt: 4 }}>
-        <Typography variant="h6">{error}</Typography>
-      </Container>
-    );
-  }
+//   if (error) {
+//     return (
+//       <Container maxWidth="100vw" sx={{ textAlign: "center", mt: 4 }}>
+//         <Typography variant="h6">{error}</Typography>
+//       </Container>
+//     );
+//   }
 
-  const formattedDate = session
-    ? format(new Date(session.created * 1000), "PPPpp")
-    : "";
+//   const formattedDate = session
+//     ? format(new Date(session.created * 1000), "PPPpp")
+//     : "";
 
-  return (
-    <Container maxWidth="100vw" sx={{ textAlign: "center", mt: 4 }}>
-      {session?.payment_status === "paid" ? (
-        <>
-          <Typography variant="h6">
-            Payment Successful, Thank you for Purchasing.
-          </Typography>
-          <Box sx={{ mt: 2 }}>
-            <Typography variant="h6">Subscription ID: {session.id}</Typography>
-            <Typography variant="h6">
-              Subscription Date: {formattedDate}
-            </Typography>
-            <Typography variant="h6">
-              Subscription Status: {session.status}
-            </Typography>
-            <Typography variant="body1">
-              We have received your payment. You will receive an email with
-              subscription details shortly.
-            </Typography>
-          </Box>
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ mt: 4 }}
-            onClick={() => router.push("/")}
-          >
-            Go to Homepage
-          </Button>
-        </>
-      ) : (
-        <>
-          <Typography variant="h4">Payment Failed</Typography>
-          <Box sx={{ mt: 4 }}>
-            <Typography variant="body1">
-              Your payment wasn&apos;t successful. Please try again later.
-            </Typography>
-          </Box>
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ mt: 4 }}
-            onClick={() => router.push("/")}
-          >
-            Retry Payment
-          </Button>
-        </>
-      )}
-    </Container>
-  );
-};
+//   return (
+//     <Container maxWidth="100vw" sx={{ textAlign: "center", mt: 4 }}>
+//       {session?.payment_status === "paid" ? (
+//         <>
+//           <Typography variant="h6">
+//             Payment Successful, Thank you for Purchasing.
+//           </Typography>
+//           <Box sx={{ mt: 2 }}>
+//             <Typography variant="h6">Subscription ID: {session.id}</Typography>
+//             <Typography variant="h6">
+//               Subscription Date: {formattedDate}
+//             </Typography>
+//             <Typography variant="h6">
+//               Subscription Status: {session.status}
+//             </Typography>
+//             <Typography variant="body1">
+//               We have received your payment. You will receive an email with
+//               subscription details shortly.
+//             </Typography>
+//           </Box>
+//           <Button
+//             variant="contained"
+//             color="primary"
+//             sx={{ mt: 4 }}
+//             onClick={() => router.push("/")}
+//           >
+//             Go to Homepage
+//           </Button>
+//         </>
+//       ) : (
+//         <>
+//           <Typography variant="h4">Payment Failed</Typography>
+//           <Box sx={{ mt: 4 }}>
+//             <Typography variant="body1">
+//               Your payment wasn&apos;t successful. Please try again later.
+//             </Typography>
+//           </Box>
+//           <Button
+//             variant="contained"
+//             color="primary"
+//             sx={{ mt: 4 }}
+//             onClick={() => router.push("/")}
+//           >
+//             Retry Payment
+//           </Button>
+//         </>
+//       )}
+//     </Container>
+//   );
+// };
 
-const ResultPage = () => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <ResultPageContent />
-  </Suspense>
-);
+// const ResultPage = () => (
+//   <Suspense fallback={<div>Loading...</div>}>
+//     <ResultPageContent />
+//   </Suspense>
+// );
 
-export default ResultPage;
+// export default ResultPage;

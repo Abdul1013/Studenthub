7552c00e56551ba8216540cc5a426255d698430d
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2024-09-30 22:02:02 +0100

    enhanced features component

--- app/FeatureSection.js
+++ app/FeatureSection.js
@@ -9,57 +9,57 @@ export const FeatureSection = () => {
       feature1:
         " Users can create flashcards by typing in questions and answers.",
       feature2:
-        "Import Content: Ability to import content from text files, PDFs, or other formats.",
+        "Ability to import content from text files, PDFs, or other formats.",
       feature3:
-        "AI-Generated Flashcards: Automatically generate flashcards from input text or documents using AI.",
+        "Automatically generate flashcards from input text or documents using AI.",
     },
     {
       id: 2,
       title: "Advanced Features",
       feature1:
-        "Contextual Learning: Use AI to provide explanations or additional context based on user questions.",
+        " Use AI to provide explanations or additional context based on user questions.",
       feature2:
-        "Adaptive Learning: Adjust study recommendations and difficulty based on user progress and performance.",
+        " Adjust study recommendations and difficulty based on user progress and performance.",
     },
 
     {
       id: 3,
       title: "Progress Tracking",
       feature1:
-        "Statistics: Track study progress with metrics like the number of flashcards reviewed, accuracy, and time spent.",
+        "Track study progress with metrics like the number of flashcards reviewed, accuracy, and time spent.",
       feature2:
-        "Performance Insights: Provide feedback and suggestions based on user performance and study habits.",
+        " Provide feedback and suggestions based on user performance and study habits.",
     },
     {
       id: 4,
       title: "Customization",
       feature1:
-        "Customizable Flashcards: Add images, audio, or links to flashcards.",
+        " Add images, audio, or links to flashcards.",
       feature2:
-        "Multiple-Choice and Fill-in-the-Blank: Support different types of flashcards, including multiple-choice and fill-in-the-blank.",
+        " Support different types of flashcards, including multiple-choice and fill-in-the-blank.",
       feature3:
-        "Theming: Allow users to customize the app’s appearance with different themes or color schemes.",
+        " Allow users to customize the app’s appearance with different themes or color schemes.",
     },
     {
       id: 5,
       title: "Study Modes",
       feature1:
-        "Review Mode: Go through flashcards one by one with the option to reveal the answer.",
+        " Go through flashcards one by one with the option to reveal the answer.",
       feature2:
         "Quiz Mode: Test users with multiple-choice questions or fill-in-the-blank questions based on flashcard content.",
       feature3:
-        "Spaced Repetition: Use algorithms to schedule flashcard reviews based on user performance and retention needs.",
+        "Use algorithms to schedule flashcard reviews based on user performance and retention needs.",
     },
 
     {
       id: 6,
       title: "Gamification",
       feature1:
-        "Achievements and Badges: Reward users with achievements and badges for reaching milestones or completing challenges.",
+        "Reward users with achievements and badges for reaching milestones or completing challenges.",
       feature2:
         "Leaderboards: Include leaderboards for competitive study sessions or challenges.",
       feature3:
-        "Collaborative Features: Share flashcard decks with others and collaborate on study materials.",
+        "Share flashcard decks with others and collaborate on study materials.",
     },
   ];
 
@@ -79,16 +79,16 @@ export const FeatureSection = () => {
             <Typography variant="h5" component="h6">
               {item.title}
             </Typography>
-            <Typography variant="body1" component="p" gutterBottom style={{textAlign: "left", fontSize: " small"}}>
+            <Typography variant="body1" component="p" gutterBottom style={{textAlign: "left", fontSize: " small", color: "#222831"}}>
               1: {item.feature1}
             </Typography>
             {item.feature2 && (
-              <Typography variant="body1" component="p" gutterBottom style={{textAlign: "left", fontSize: " small"}}>
+              <Typography variant="body1" component="p" gutterBottom style={{textAlign: "left", fontSize: " small", color: "#222831"}}>
                 2: {item.feature2}
               </Typography>
             )}
             {item.feature3 && (
-              <Typography variant="body1" component="p" style={{textAlign: "left", fontSize: " small"}}>
+              <Typography variant="body1" component="p" style={{textAlign: "left", fontSize: " small", color: "#222831"}}>
                 3: {item.feature3}
               </Typography>
             )}
